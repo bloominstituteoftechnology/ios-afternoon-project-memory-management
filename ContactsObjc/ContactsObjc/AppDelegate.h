31015f7c3bd51481756f2f2076ac475205572cef
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-07-24 15:57:18 -0400

    Setup Storyboard and disabled automatic reference counting

---
+++ ContactsObjc/ContactsObjc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ContactsObjc
+//
+//  Created by Thomas Cacciatore on 7/24/19.
+//  Copyright © 2019 Thomas Cacciatore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
