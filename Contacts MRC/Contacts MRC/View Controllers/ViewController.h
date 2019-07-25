8d954bdc6608941bc9a5c057cb16667777f97702
hectorsvill <hectorstevenvillasano@gmail.com>
2019-07-25 13:16:34 -0700

    init

---
+++ Contacts MRC/Contacts MRC/View Controllers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Contacts MRC
+//
+//  Created by Hector Steven on 7/25/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Contacts MRC/Contacts MRC/View Controllers/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Contacts MRC
+//
+//  Created by Hector Steven on 7/25/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+
+@end
