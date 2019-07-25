47644f9652357bc1c12da0f337e593e2d98c9ebf
hectorsvill <hectorstevenvillasano@gmail.com>
2019-07-25 15:28:24 -0700

    detail view outlets

--- Contacts MRC/Contacts MRC/View Controllers/HSVContactDetailViewController.m
+++ Contacts MRC/Contacts MRC/View Controllers/HSVContactDetailViewController.m
@@ -14,19 +14,25 @@
 
 @implementation HSVContactDetailViewController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)addUpdateButtonPressed:(id)sender {
+	printf("the button was pressed");
+	
 }
-*/
+
+
+- (void)dealloc {
+	[_namTextField release];
+	[_emailTextField release];
+	[_phoneNumberTextField release];
+	[_addUpdateButton release];
+	[super dealloc];
+}
+
 
 @end
