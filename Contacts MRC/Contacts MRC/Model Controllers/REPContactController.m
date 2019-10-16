4ba72030dfa9b1fd62b4b4bf56fe151b9db2d223
Michael Redig <mredig@gmail.com>
2019-10-16 15:21:09 -0500

    added model files

---
+++ Contacts MRC/Contacts MRC/Model Controllers/REPContactController.m
@@ -0,0 +1,13 @@
+//
+//  REPContactController.m
+//  Contacts MRC
+//
+//  Created by Michael Redig on 10/16/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+#import "REPContactController.h"
+
+@implementation REPContactController
+
+@end
