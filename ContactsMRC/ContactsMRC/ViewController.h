01e6d55a35860f02599e9b1790670cefa0fa85fa
ladybeitel <ciarabeitel@gmail.com>
2019-11-13 18:21:25 -0500

    Turn off ARC. Edit bug in main.m file.

---
+++ ContactsMRC/ContactsMRC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ContactsMRC
+//
+//  Created by Ciara Beitel on 11/13/19.
+//  Copyright © 2019 Ciara Beitel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
