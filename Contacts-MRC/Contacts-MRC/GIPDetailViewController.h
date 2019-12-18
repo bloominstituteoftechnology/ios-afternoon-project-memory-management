26969e45c07796737ecf2953c32430c6fb8de18d
Gi Pyo Kim <gipyok@gmail.com>
2019-12-18 13:11:58 -0800

    Basic UI setup

---
+++ Contacts-MRC/Contacts-MRC/GIPDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  GIPDetailViewController.h
+//  Contacts-MRC
+//
+//  Created by Gi Pyo Kim on 12/18/19.
+//  Copyright © 2019 GIPGIP Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GIPDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Contacts-MRC/Contacts-MRC/GIPDetailViewController.m
@@ -0,0 +1,41 @@
+//
+//  GIPDetailViewController.m
+//  Contacts-MRC
+//
+//  Created by Gi Pyo Kim on 12/18/19.
+//  Copyright © 2019 GIPGIP Studio. All rights reserved.
+//
+
+#import "GIPDetailViewController.h"
+
+@interface GIPDetailViewController ()
+@property (retain, nonatomic) IBOutlet UITextField *nameTextField;
+@property (retain, nonatomic) IBOutlet UITextField *emailTextField;
+@property (retain, nonatomic) IBOutlet UITextField *phoneNumberTextField;
+
+@end
+
+@implementation GIPDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (void)dealloc {
+    [_nameTextFIeld release];
+    [_emailTextField release];
+    [_phoneNumberTextField release];
+    [super dealloc];
+}
+@end
