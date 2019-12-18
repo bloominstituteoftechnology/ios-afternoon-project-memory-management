ec7aba5cc6b0442aae17d7d697f9a24078dacef9
Gi Pyo Kim <gipyok@gmail.com>
2019-12-18 12:51:37 -0800

    initial commit

---
+++ Contacts-MRC/Contacts-MRC/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Contacts-MRC
+//
+//  Created by Gi Pyo Kim on 12/18/19.
+//  Copyright © 2019 GIPGIP Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Contacts-MRC/Contacts-MRC/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Contacts-MRC
+//
+//  Created by Gi Pyo Kim on 12/18/19.
+//  Copyright © 2019 GIPGIP Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
