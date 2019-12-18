4f5f7a83308de60e45d1af9d87c679587bd930a3
Gi Pyo Kim <gipyok@gmail.com>
2019-12-18 15:22:17 -0800

    Fixed the contact controller internal array to have no memory leak

--- Contacts-MRC/Contacts-MRC/Model Controllers/GIPContactController.m
+++ Contacts-MRC/Contacts-MRC/Model Controllers/GIPContactController.m
@@ -22,17 +22,24 @@
     self = [super init];
     if (self) {
         
-        _internalContacts = [[[NSMutableArray alloc] initWithObjects:
-                              [[GIPContact alloc] initWithName:@"John" email:@"johngk93@gmail.com" phoneNumber:@"7143368822"],
-                              [[GIPContact alloc] initWithName:@"Steve" email:@"stebong@gmail.com" phoneNumber:@"5627145544"],
-                              nil] autorelease];
+        _internalContacts = [[NSMutableArray alloc] initWithObjects:
+                              [[[GIPContact alloc] initWithName:@"John" email:@"johngk93@gmail.com" phoneNumber:@"7143368822"] autorelease],
+                              [[[GIPContact alloc] initWithName:@"Steve" email:@"stebong@gmail.com" phoneNumber:@"5627145544"] autorelease],
+                              nil];
     }
     return self;
 }
 
 - (NSArray<GIPContact *> *)contacts {
-    return [self.internalContacts retain];
+    return [[self.internalContacts retain] autorelease];
 }
+
+//- (void)setContacts:(NSArray<GIPContact *> *)contacts {
+//    if (self.contacts != contacts) {
+//        [self.contacts release];
+//        self.contacts = [contacts retain];
+//    }
+//}
 
 - (void)addContact:(GIPContact *)contact {
     [self.internalContacts addObject:contact];
@@ -42,6 +49,7 @@
     GIPContact *newContact = [[GIPContact alloc] initWithName:name email:email phoneNumber:phoneNumber];
     NSInteger index = [self.internalContacts indexOfObject:contact];
     self.internalContacts[index] = newContact;
+    [newContact release];
 }
 
 - (void)removeContact:(GIPContact *)contact {
@@ -50,7 +58,10 @@
 }
 
 - (void)dealloc {
-    
+    [_contacts release];
+    [_internalContacts release];
+    _contacts = nil;
+    _internalContacts = nil;
     [super dealloc];
 }
 
