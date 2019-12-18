760c93eb9673ea89302b372caf8718c015a90e71
Isvvc <lyons@tuta.io>
2019-12-18 13:38:16 -0700

    Create LSIContact class

---
+++ Contacts MRC/Contacts MRC/Model/LSIContact.h
@@ -0,0 +1,17 @@
+//
+//  LSIContact.h
+//  Contacts MRC
+//
+//  Created by Isaac Lyons on 12/18/19.
+//  Copyright © 2019 Isaac Lyons. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIContact : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Contacts MRC/Contacts MRC/Model/LSIContact.m
@@ -0,0 +1,13 @@
+//
+//  LSIContact.m
+//  Contacts MRC
+//
+//  Created by Isaac Lyons on 12/18/19.
+//  Copyright © 2019 Isaac Lyons. All rights reserved.
+//
+
+#import "LSIContact.h"
+
+@implementation LSIContact
+
+@end
