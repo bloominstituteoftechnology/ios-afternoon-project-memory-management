3a8e38e5803f77e382a324096ef5fe8bd6a9f771
Isvvc <lyons@tuta.io>
2019-12-18 13:16:51 -0700

    Create blank Xcode project

---
+++ Contacts MRC/Contacts MRC/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Contacts MRC
+//
+//  Created by Isaac Lyons on 12/18/19.
+//  Copyright © 2019 Isaac Lyons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Contacts MRC/Contacts MRC/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Contacts MRC
+//
+//  Created by Isaac Lyons on 12/18/19.
+//  Copyright © 2019 Isaac Lyons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
