baea5c5d6b2972dbdc374fdd65e2bc43186801ac
Wilma <kima_92@fastmail.com>
2020-01-29 15:22:42 -0500

    Initial Commit

---
+++ Contacts MRC Objc/Contacts MRC Objc/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Contacts MRC Objc
+//
+//  Created by macbook on 1/29/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
