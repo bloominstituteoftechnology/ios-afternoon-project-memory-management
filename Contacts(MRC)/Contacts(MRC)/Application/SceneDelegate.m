6feb023d495985ee576e2b110bbf0150e1014c53
Jon Bash <jonbash@protonmail.com>
2020-01-29 13:34:31 -0800

    fix app/scene delegate memory leaks

--- Contacts(MRC)/Contacts(MRC)/Application/SceneDelegate.m
+++ Contacts(MRC)/Contacts(MRC)/Application/SceneDelegate.m
@@ -6,7 +6,6 @@
 
 @implementation SceneDelegate
 
-
 - (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions {
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
@@ -46,5 +45,9 @@
     // to restore the scene back to its current state.
 }
 
-
+- (void)dealloc
+{
+    [_window release];
+    [super dealloc];
+}
 @end
