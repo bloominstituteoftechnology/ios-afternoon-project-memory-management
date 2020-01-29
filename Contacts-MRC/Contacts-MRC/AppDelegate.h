4484f6ea167c7979ac5bfdb90dc1fa59024a64a4
Percy <percival.ngan@gmail.com>
2020-01-29 15:32:38 -0600

    Added starter project files

---
+++ Contacts-MRC/Contacts-MRC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Contacts-MRC
+//
+//  Created by Percy Ngan on 1/29/20.
+//  Copyright © 2020 Lamdba School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Contacts-MRC/Contacts-MRCTests/Contacts_MRCTests.m
@@ -0,0 +1,37 @@
+//
+//  Contacts_MRCTests.m
+//  Contacts-MRCTests
+//
+//  Created by Percy Ngan on 1/29/20.
+//  Copyright © 2020 Lamdba School. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Contacts_MRCTests : XCTestCase
+
+@end
+
+@implementation Contacts_MRCTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
