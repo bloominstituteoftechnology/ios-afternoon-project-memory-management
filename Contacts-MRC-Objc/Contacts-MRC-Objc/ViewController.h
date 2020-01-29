cf0a009eaa2c68869d941ceabb8a7192fc03c636
Skyler Suzuki <skylersuzuk1@gmail.com>
2020-01-29 12:48:32 -0800

    Base project.

---
+++ Contacts-MRC-Objc/Contacts-MRC-Objc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Contacts-MRC-Objc
+//
+//  Created by Lambda_School_Loaner_204 on 1/29/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Contacts-MRC-Objc/Contacts-MRC-Objc/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Contacts-MRC-Objc
+//
+//  Created by Lambda_School_Loaner_204 on 1/29/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
