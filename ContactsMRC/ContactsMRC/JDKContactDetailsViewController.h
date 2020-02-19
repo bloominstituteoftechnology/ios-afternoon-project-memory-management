9892b80f154c09b37dc2bd6c5bd6168badd56dd6
John Kouris <jdkouris@gmail.com>
2020-02-18 18:49:23 -0800

    Designed the UI with storyboards and added the view controller files

--- ContactsMRC/ContactsMRC/JDKContactDetailsViewController.h
+++ ContactsMRC/ContactsMRC/JDKContactDetailsViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  JDKContactDetailsViewController.h
 //  ContactsMRC
 //
 //  Created by John Kouris on 2/18/20.
@@ -8,8 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+NS_ASSUME_NONNULL_BEGIN
 
+@interface JDKContactDetailsViewController : UIViewController
 
 @end
 
+NS_ASSUME_NONNULL_END
---
+++ ContactsMRC/ContactsMRC/JDKContactsTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  JDKContactsTableViewController.h
+//  ContactsMRC
+//
+//  Created by John Kouris on 2/18/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JDKContactsTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
