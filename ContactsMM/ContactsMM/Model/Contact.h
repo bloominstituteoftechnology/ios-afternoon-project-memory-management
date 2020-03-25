22670d568c0a66e5fe99d69d7b3f6f0538244266
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-03-25 14:19:46 -0700

    model done

--- ContactsMM/ContactsMM/Model/Contact.h
+++ ContactsMM/ContactsMM/Model/Contact.h
@@ -12,6 +12,21 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface Contact : NSObject
 
+// MARK: - Properties
+
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *email;
+@property (nonatomic, copy) NSString *phone;
+
+// MARK: - Initializers
+
+- (instancetype)initWithName:(NSString *)name
+                       email:(NSString *)email
+                       phone:(NSString *)phone;
+
++ (instancetype)contactWithName:(NSString *)name
+                          email:(NSString *)email
+                          phone:(NSString *)phone;
 @end
 
 NS_ASSUME_NONNULL_END
