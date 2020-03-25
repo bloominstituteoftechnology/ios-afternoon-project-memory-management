76cf24c295f0c99e0d8ee16b74c997f85017332a
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-03-25 12:38:31 -0700

    added project

---
+++ ContactsMM/ContactsMM/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ContactsMM
+//
+//  Created by Jorge Alvarez on 3/25/20.
+//  Copyright © 2020 Jorge Alvarez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ContactsMM/ContactsMM/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ContactsMM
+//
+//  Created by Jorge Alvarez on 3/25/20.
+//  Copyright © 2020 Jorge Alvarez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"Howdy");
+}
+
+
+@end
