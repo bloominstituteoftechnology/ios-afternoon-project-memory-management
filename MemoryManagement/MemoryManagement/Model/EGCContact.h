c9dfc0a7355d0cecd4c943a52f542f4771842841
Enrique Gongora <EGTech24@gmail.com>
2020-04-22 13:49:11 -0700

    Storyboard completed. Model files created.

---
+++ MemoryManagement/MemoryManagement/Model/EGCContact.h
@@ -0,0 +1,17 @@
+//
+//  EGCContact.h
+//  MemoryManagement
+//
+//  Created by Enrique Gongora on 4/22/20.
+//  Copyright © 2020 Enrique Gongora. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EGCContact : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MemoryManagement/MemoryManagement/Model/EGCContact.m
@@ -0,0 +1,13 @@
+//
+//  EGCContact.m
+//  MemoryManagement
+//
+//  Created by Enrique Gongora on 4/22/20.
+//  Copyright © 2020 Enrique Gongora. All rights reserved.
+//
+
+#import "EGCContact.h"
+
+@implementation EGCContact
+
+@end
