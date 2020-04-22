e21a3c56a6874edfefb83377f05a4f2f88e31eb6
scottharris86 <scottharrisdev@gmail.com>
2020-04-22 15:28:17 -0400

    Disable ARC First Fixed Xcode 11 Bug

---
+++ ContactsMRC/ContactsMRC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ContactsMRC
+//
+//  Created by scott harris on 4/22/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
