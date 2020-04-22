30eff67dd7386cd66f04c8326ded66da2338b59e
Keri Levesque <kerianne166@gmail.com>
2020-04-22 18:58:37 -0400

    added model

---
+++ MemoryManagement/MemoryManagement/Model/KMLContact.m
@@ -0,0 +1,13 @@
+//
+//  KMLContact.m
+//  MemoryManagement
+//
+//  Created by Keri Levesque on 4/22/20.
+//  Copyright © 2020 Keri Levesque. All rights reserved.
+//
+
+#import "KMLContact.h"
+
+@implementation KMLContact
+
+@end
