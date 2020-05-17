972ee84449206afa5d91ad0f6eee696bf4adb7d8
Hector <hectoremil21@hotmail.com>
2020-05-17 17:39:20 -0400

    Fix main.m

---
+++ Contacts MRC/Contacts MRC/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Contacts MRC
+//
+//  Created by Karen Rodriguez on 5/17/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Contacts MRC/Contacts MRC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Contacts MRC
+//
+//  Created by Karen Rodriguez on 5/17/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
