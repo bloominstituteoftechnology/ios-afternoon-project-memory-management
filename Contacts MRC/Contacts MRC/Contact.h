a4bceac6441167916fcee28f0727da107c9ce2bf
Hector <hectoremil21@hotmail.com>
2020-05-17 17:59:13 -0400

    Contact header file edit. Add properties and initializer.

--- Contacts MRC/Contacts MRC/Contact.h
+++ Contacts MRC/Contacts MRC/Contact.h
@@ -8,10 +8,13 @@
 
 #import <Foundation/Foundation.h>
 
-NS_ASSUME_NONNULL_BEGIN
-
 @interface Contact : NSObject
+
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *email;
+@property (nonatomic, copy) NSString *phoneNumber;
+
+- (instancetype)initWithName:(NSString *)name email:(NSString *)email phoneNumber:(NSString *)phoneNumber;
 
 @end
 
-NS_ASSUME_NONNULL_END
