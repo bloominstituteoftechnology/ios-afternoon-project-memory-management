a752cff9cd496386ebe66ed733e9dfa6acf5c8b9
Wyatt Harrell <wyattaharrell@gmail.com>
2020-05-20 16:08:47 -0400

    Created initial Xcode project

---
+++ Contacts/Contacts/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Contacts
+//
+//  Created by Wyatt Harrell on 5/20/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
