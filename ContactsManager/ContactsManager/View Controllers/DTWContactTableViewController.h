c27c0c1b50959692089a508c8a44d9a4025410b6
David Wright <davidtwright89@gmail.com>
2020-06-10 05:05:14 -0400

    Added a DTWContactTableViewController

---
+++ ContactsManager/ContactsManager/View Controllers/DTWContactTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  DTWContactTableViewController.h
+//  ContactsManager
+//
+//  Created by David Wright on 6/10/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DTWContactTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
