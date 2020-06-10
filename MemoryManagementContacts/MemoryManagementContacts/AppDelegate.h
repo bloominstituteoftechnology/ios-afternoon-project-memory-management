458836ff70f7cd95c71340e7d88ff597c755bd35
Sal <salmanamer@gmail.com>
2020-06-10 02:58:37 -0700

    setup Project

---
+++ MemoryManagementContacts/MemoryManagementContacts/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MemoryManagementContacts
+//
+//  Created by Sal B Amer on 6/10/20.
+//  Copyright © 2020 Sal B Amer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MemoryManagementContacts/MemoryManagementContacts/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MemoryManagementContacts
+//
+//  Created by Sal B Amer on 6/10/20.
+//  Copyright © 2020 Sal B Amer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
