cf369f619bbb2de706e55821bd29ee9b103a76d5
David Wright <davidtwright89@gmail.com>
2020-06-10 05:07:50 -0400

    Added a DTWContactController model controller class

---
+++ ContactsManager/ContactsManager/Model Controller/DTWContactController.h
@@ -0,0 +1,17 @@
+//
+//  DTWContactController.h
+//  ContactsManager
+//
+//  Created by David Wright on 6/10/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DTWContactController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ContactsManager/ContactsManager/Model Controller/DTWContactController.m
@@ -0,0 +1,13 @@
+//
+//  DTWContactController.m
+//  ContactsManager
+//
+//  Created by David Wright on 6/10/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import "DTWContactController.h"
+
+@implementation DTWContactController
+
+@end
