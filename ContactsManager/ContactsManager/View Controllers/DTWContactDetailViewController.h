647bd9f8244493f35582e56ac12dd212299ddb44
David Wright <davidtwright89@gmail.com>
2020-06-10 05:05:57 -0400

    Added a DTWContactDetailViewController

---
+++ ContactsManager/ContactsManager/View Controllers/DTWContactDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DTWContactDetailViewController.h
+//  ContactsManager
+//
+//  Created by David Wright on 6/10/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DTWContactDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ContactsManager/ContactsManager/View Controllers/DTWContactDetailViewController.m
@@ -0,0 +1,32 @@
+//
+//  DTWContactDetailViewController.m
+//  ContactsManager
+//
+//  Created by David Wright on 6/10/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import "DTWContactDetailViewController.h"
+
+@interface DTWContactDetailViewController ()
+
+@end
+
+@implementation DTWContactDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
