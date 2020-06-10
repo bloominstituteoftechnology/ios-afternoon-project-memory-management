8b56b26dd1b87108222960707c4c1b5d88a2376d
David Wright <davidtwright89@gmail.com>
2020-06-10 05:04:24 -0400

    Added a DTWContact model

---
+++ ContactsManager/ContactsManager/Model/DTWContact.h
@@ -0,0 +1,17 @@
+//
+//  DTWContact.h
+//  ContactsManager
+//
+//  Created by David Wright on 6/10/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DTWContact : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ContactsManager/ContactsManager/Model/DTWContact.m
@@ -0,0 +1,13 @@
+//
+//  DTWContact.m
+//  ContactsManager
+//
+//  Created by David Wright on 6/10/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import "DTWContact.h"
+
+@implementation DTWContact
+
+@end
