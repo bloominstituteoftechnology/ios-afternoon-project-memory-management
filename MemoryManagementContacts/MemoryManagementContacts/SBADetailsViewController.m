6cda3a09e3f6ac3381ec96ab96db4a65bdeaf8f5
Sal <salmanamer@gmail.com>
2020-06-10 04:15:37 -0700

    finished SBADetailsViewController.m

--- MemoryManagementContacts/MemoryManagementContacts/SBADetailsViewController.m
+++ MemoryManagementContacts/MemoryManagementContacts/SBADetailsViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "SBADetailsViewController.h"
+#import "SBAContactController.h"
+#import "SBAContact.h"
 
 @interface SBADetailsViewController ()
+//IBOutlets
+@property (retain, nonatomic) IBOutlet UITextField *nameField;
+@property (retain, nonatomic) IBOutlet UITextField *emailField;
+@property (retain, nonatomic) IBOutlet UITextField *phoneField;
 
 @end
 
@@ -16,17 +22,35 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    if (self.contact) {
+        self.nameField.text = self.contact.name;
+        self.emailField.text = self.contact.email;
+        self.phoneField.text = self.contact.phone;
+    }
 }
 
-/*
-#pragma mark - Navigation
+ 
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+
+//IB ACtion Save Button
+- (IBAction)saveBtnWasPressed:(UIBarButtonItem *)sender {
+    if (self.contact) {
+        self.contact.name  = self.nameField.text;
+        self.contact.email = self.emailField.text;
+        self.contact.phone = self.phoneField.text;
+    } else {
+        [self.contactController newContactWithName:self.nameField.text email:self.emailField.text phone:self.phoneField.text];
+    }
+    [self.navigationController popViewControllerAnimated:true];
 }
-*/
 
+
+ 
+
+- (void)dealloc {
+    [_nameField release];
+    [_emailField release];
+    [_phoneField release];
+    [super dealloc];
+}
 @end
