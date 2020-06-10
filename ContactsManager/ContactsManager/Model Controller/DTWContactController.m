e8e721a313044783c6d52eda47eadf2682851d37
David Wright <davidtwright89@gmail.com>
2020-06-10 06:16:44 -0400

    Started implementing DTWContactController

--- ContactsManager/ContactsManager/Model Controller/DTWContactController.m
+++ ContactsManager/ContactsManager/Model Controller/DTWContactController.m
@@ -7,7 +7,28 @@
 //
 
 #import "DTWContactController.h"
+#import "DTWContact.h"
+
+@interface DTWContactController ()
+
+@property (nonatomic) NSMutableArray *internalContacts;
+
+@end
 
 @implementation DTWContactController
+
+@synthesize contacts = _contacts;
+- (NSArray<DTWContact *> *)contacts
+{
+    if (!_contacts) {
+        //_contacts = [[NSArray alloc] init];
+        _contacts = @[
+            [[DTWContact alloc] initWithName:@"David Wright" Email:@"david.wright@email.com" PhoneNumber:@"123-456-7890"],
+            [[DTWContact alloc] initWithName:@"Katie Wright" Email:@"katie.wright@email.com" PhoneNumber:@"234-789-1560"],
+            [[DTWContact alloc] initWithName:@"Tim Apple" Email:@"tim.apple@email.com" PhoneNumber:@"555-1029-1560"],
+        ];
+    }
+    return _contacts;
+}
 
 @end
