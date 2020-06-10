0da8afaecbd4e4039330f61e41a8fc0d5ddae88a
David Wright <davidtwright89@gmail.com>
2020-06-10 04:11:34 -0400

    Created a new Xcode project and organized the project files

---
+++ ContactsManager/ContactsManager/Resources/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ContactsManager
+//
+//  Created by David Wright on 6/10/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ContactsManager/ContactsManager/Resources/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  ContactsManager
+//
+//  Created by David Wright on 6/10/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
