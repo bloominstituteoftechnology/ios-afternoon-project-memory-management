8f87cbe292800884344e3ea1635db32e043abe56
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-06-17 12:54:17 -0700

    disabled ARC

---
+++ MRC-project/MRC-project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MRC-project
+//
+//  Created by Kenny on 6/17/20.
+//  Copyright © 2020 Hazy Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MRC-project/MRC-project/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MRC-project
+//
+//  Created by Kenny on 6/17/20.
+//  Copyright © 2020 Hazy Studios. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
