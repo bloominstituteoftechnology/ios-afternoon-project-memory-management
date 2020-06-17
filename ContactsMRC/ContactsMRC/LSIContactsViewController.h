d4f4701b599e72159aa549ee9817b14c29029d7e
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-17 13:49:14 -0700

    Added the detail view controller & table view controller

---
+++ ContactsMRC/ContactsMRC/LSIContactsViewController.h
@@ -0,0 +1,17 @@
+//
+//  LSIContactsViewController.h
+//  ContactsMRC
+//
+//  Created by Bhawnish Kumar on 6/17/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIContactsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ContactsMRC/ContactsMRC/LSIContactsViewController.m
@@ -0,0 +1,32 @@
+//
+//  LSIContactsViewController.m
+//  ContactsMRC
+//
+//  Created by Bhawnish Kumar on 6/17/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+#import "LSIContactsViewController.h"
+
+@interface LSIContactsViewController ()
+
+@end
+
+@implementation LSIContactsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
