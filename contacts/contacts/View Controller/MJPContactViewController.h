0dbdaaa7a9afed51b7cf47240dd041f045c02a60
Mark Poggi <markp@mac.com>
2020-06-17 12:51:41 -0700

    Set-up UI and created associated view controller files

---
+++ contacts/contacts/View Controller/MJPContactViewController.h
@@ -0,0 +1,17 @@
+//
+//  MJPContactViewController.h
+//  contacts
+//
+//  Created by Mark Poggi on 6/17/20.
+//  Copyright © 2020 Mark Poggi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MJPContactViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ contacts/contacts/View Controller/MJPContactViewController.m
@@ -0,0 +1,32 @@
+//
+//  MJPContactViewController.m
+//  contacts
+//
+//  Created by Mark Poggi on 6/17/20.
+//  Copyright © 2020 Mark Poggi. All rights reserved.
+//
+
+#import "MJPContactViewController.h"
+
+@interface MJPContactViewController ()
+
+@end
+
+@implementation MJPContactViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
