c5c932b620860439863f5322cf4373bdbd14cd22
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-06-17 13:18:44 -0700

    Fixed compliation issue after moving resources folder

---
+++ MRC-project/MRC-project/Model/HSIContact.m
@@ -0,0 +1,15 @@
+//
+//  HSIContact.m
+//  MRC-project
+//
+//  Created by Kenny on 6/17/20.
+//  Copyright © 2020 Hazy Studios. All rights reserved.
+//
+
+#import "HSIContact.h"
+
+@implementation HSIContact
+
+
+
+@end
