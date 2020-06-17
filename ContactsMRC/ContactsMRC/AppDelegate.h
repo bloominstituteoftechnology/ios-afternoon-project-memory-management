1cecc7e076da98f5cc56479e3ba1cecf187878b8
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-17 13:14:00 -0700

    Added the projec for MRC

---
+++ ContactsMRC/ContactsMRC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ContactsMRC
+//
+//  Created by Bhawnish Kumar on 6/17/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
