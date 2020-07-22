b1c61e67fa9e7c0a18cbc627afbf67cf14526c91
nonye <nnezekwo@gmail.com>
2020-07-22 16:58:46 -0700

    Late start, first commit

---
+++ AfternoonMemoryManagement/AfternoonMemoryManagement/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AfternoonMemoryManagement
+//
+//  Created by Nonye on 7/22/20.
+//  Copyright © 2020 Nonye Ezekwo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ AfternoonMemoryManagement/AfternoonMemoryManagement/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  AfternoonMemoryManagement
+//
+//  Created by Nonye on 7/22/20.
+//  Copyright © 2020 Nonye Ezekwo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
