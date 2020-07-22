ec1dc43398aff16e13628e951996acc44c82c201
Cody Morley <codymorleyinc@gmail.com>
2020-07-22 12:59:45 -0700

    Global - Project Outline
    Set project outline for objective-c project Contacts. Turned off Automatic Reference Counting. Don't do this at home kids.

---
+++ Contacts/Contacts/Application/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Contacts
+//
+//  Created by Cody Morley on 7/22/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Contacts/Contacts/View Controllers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Contacts
+//
+//  Created by Cody Morley on 7/22/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
