0e71e9d2efae8b752da35b60b3b604b7ac848efd
Cody Morley <codymorleyinc@gmail.com>
2020-07-22 15:05:48 -0700

    CAMContactController.h/.m
    Created model controller file and added to the project with model imported and a class declaration in the header.

---
+++ Contacts/Contacts/Model Controllers/CAMContactController.h
@@ -0,0 +1,18 @@
+//
+//  CAMContactController.h
+//  Contacts
+//
+//  Created by Cody Morley on 7/22/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+@class CAMContact;
+
+@interface CAMContactController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Contacts/Contacts/Model Controllers/CAMContactController.m
@@ -0,0 +1,14 @@
+//
+//  CAMContactController.m
+//  Contacts
+//
+//  Created by Cody Morley on 7/22/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+#import "CAMContactController.h"
+#import "CAMContact.h"
+
+@implementation CAMContactController
+
+@end
