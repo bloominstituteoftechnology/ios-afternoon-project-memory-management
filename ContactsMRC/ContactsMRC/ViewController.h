055bd8b0de7708de742a7c83027ee29c5afceece
Kelson Hartle <kelson99hartle@icloud.com>
2020-07-30 14:54:42 -0600

    added xcode project

---
+++ ContactsMRC/ContactsMRC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ContactsMRC
+//
+//  Created by Kelson Hartle on 7/30/20.
+//  Copyright © 2020 Kelson Hartle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ContactsMRC/ContactsMRC/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ContactsMRC
+//
+//  Created by Kelson Hartle on 7/30/20.
+//  Copyright © 2020 Kelson Hartle. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
