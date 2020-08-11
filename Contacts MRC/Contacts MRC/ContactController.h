7850cbeb9ac355176d4a5cdfe9b9a09b67445334
seanacres <6416690+seanacres@users.noreply.github.com>
2020-08-11 14:56:30 -0400

    Added contact controller files

---
+++ Contacts MRC/Contacts MRC/ContactController.h
@@ -0,0 +1,17 @@
+//
+//  ContactController.h
+//  Contacts MRC
+//
+//  Created by Sean Acres on 8/11/20.
+//  Copyright © 2020 Sean Acres. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ContactController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Contacts MRC/Contacts MRC/ContactController.m
@@ -0,0 +1,13 @@
+//
+//  ContactController.m
+//  Contacts MRC
+//
+//  Created by Sean Acres on 8/11/20.
+//  Copyright © 2020 Sean Acres. All rights reserved.
+//
+
+#import "ContactController.h"
+
+@implementation ContactController
+
+@end
