271dbea590ea0c5170344c8f9c55b58720361626
seanacres <6416690+seanacres@users.noreply.github.com>
2020-08-11 14:15:40 -0400

    Added files for UI and set up outlets

---
+++ Contacts MRC/Contacts MRC/ContactDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  ContactDetailViewController.h
+//  Contacts MRC
+//
+//  Created by Sean Acres on 8/11/20.
+//  Copyright © 2020 Sean Acres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ContactDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Contacts MRC/Contacts MRC/ContactDetailViewController.m
@@ -0,0 +1,42 @@
+//
+//  ContactDetailViewController.m
+//  Contacts MRC
+//
+//  Created by Sean Acres on 8/11/20.
+//  Copyright © 2020 Sean Acres. All rights reserved.
+//
+
+#import "ContactDetailViewController.h"
+
+@interface ContactDetailViewController ()
+
+@property (retain, nonatomic) IBOutlet UITextField *contactNameTextField;
+@property (retain, nonatomic) IBOutlet UITextField *contactEmailAddressTextField;
+@property (retain, nonatomic) IBOutlet UITextField *contactPhoneNumberTextField;
+
+@end
+
+@implementation ContactDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (void)dealloc {
+    [_contactNameTextField release];
+    [_contactEmailAddressTextField release];
+    [_contactPhoneNumberTextField release];
+    [super dealloc];
+}
+@end
