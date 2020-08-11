8444421b92c2dca2a5b2a91d28a5c4484f195e80
seanacres <6416690+seanacres@users.noreply.github.com>
2020-08-11 14:18:14 -0400

    Set up contact model object files

---
+++ Contacts MRC/Contacts MRC/Contact.h
@@ -0,0 +1,17 @@
+//
+//  Contact.h
+//  Contacts MRC
+//
+//  Created by Sean Acres on 8/11/20.
+//  Copyright © 2020 Sean Acres. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Contact : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Contacts MRC/Contacts MRC/Contact.m
@@ -0,0 +1,13 @@
+//
+//  Contact.m
+//  Contacts MRC
+//
+//  Created by Sean Acres on 8/11/20.
+//  Copyright © 2020 Sean Acres. All rights reserved.
+//
+
+#import "Contact.h"
+
+@implementation Contact
+
+@end
