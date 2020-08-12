5a24c6119cb7427d176d719e7732557a7907db77
Chad Parker <chad@chadparker.com>
2020-08-11 19:47:16 -0700

    Create ContactsController

---
+++ ContactsMRC/ContactsController.h
@@ -0,0 +1,19 @@
+//
+//  ContactsController.h
+//  ContactsMRC
+//
+//  Created by Chad Parker on 8/11/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ContactsController : NSObject
+
+@property (nonatomic, readonly) NSUInteger contactCount;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ContactsMRC/ContactsController.m
@@ -0,0 +1,19 @@
+//
+//  ContactsController.m
+//  ContactsMRC
+//
+//  Created by Chad Parker on 8/11/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+#import "ContactsController.h"
+
+@interface ContactsController ()
+
+@property (nonatomic) NSMutableArray *contacts;
+
+@end
+
+@implementation ContactsController
+
+@end
