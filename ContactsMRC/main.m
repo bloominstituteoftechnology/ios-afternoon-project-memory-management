39049711090d443824e626f5344f3d40a89c09b8
Chad Parker <chad@chadparker.com>
2020-08-11 19:22:19 -0700

    Disable ARC

--- ContactsMRC/main.m
+++ ContactsMRC/main.m
@@ -10,10 +10,10 @@
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
-    NSString * appDelegateClassName;
     @autoreleasepool {
+        NSString * appDelegateClassName;
         // Setup code that might create autoreleased objects goes here.
         appDelegateClassName = NSStringFromClass([AppDelegate class]);
+        return UIApplicationMain(argc, argv, nil, appDelegateClassName);
     }
-    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
 }
