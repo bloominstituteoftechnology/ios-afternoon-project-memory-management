23f7f2d7e2b0400e12da1c282129e2c2518aff33
Claudia Maciel <clc_80@yahoo.com>
2020-08-11 18:08:22 -0700

    Added new project

---
+++ Contacts/Contacts/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Contacts
+//
+//  Created by Claudia Maciel on 8/11/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Contacts/Contacts/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Contacts
+//
+//  Created by Claudia Maciel on 8/11/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
