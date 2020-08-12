0c13d276055598db143f3509fc44e26fc8cccfe0
Matt Martindale <martindalematt18@yahoo.com>
2020-08-11 19:22:25 -0600

    Setup Storyboard

---
+++ Contacts MRC/Contacts MRC/ContactsTableViewController.m
@@ -0,0 +1,54 @@
+//
+//  ContactsTableViewController.m
+//  Contacts MRC
+//
+//  Created by Matthew Martindale on 8/11/20.
+//  Copyright © 2020 Matthew Martindale. All rights reserved.
+//
+
+#import "ContactsTableViewController.h"
+
+@interface ContactsTableViewController ()
+
+@end
+
+@implementation ContactsTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Incomplete implementation, return the number of sections
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete implementation, return the number of rows
+    return 0;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ContactCell" forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+
+@end
---
+++ Contacts MRC/Contacts MRC/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Contacts MRC
+//
+//  Created by Matthew Martindale on 8/11/20.
+//  Copyright © 2020 Matthew Martindale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
