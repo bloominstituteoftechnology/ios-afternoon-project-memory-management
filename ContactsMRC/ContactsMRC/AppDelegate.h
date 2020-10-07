96712b99b00596711d328fec9b0507d110b80593
kensam1 <samarakodykennedy@gmail.com>
2020-10-06 23:49:49 -0400

    Setup UI

---
+++ ContactsMRC/ContactsMRC/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ContactsMRC
+//
+//  Created by Lambda_School_Loaner_241 on 10/6/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ContactsMRC/ContactsMRC/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  ContactsMRC
+//
+//  Created by Lambda_School_Loaner_241 on 10/6/20.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
+
+
+// Xcode 11 Bug part
+
+// come back 
