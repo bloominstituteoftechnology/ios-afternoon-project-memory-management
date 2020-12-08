ac397fea284da90521bb7b7c91876f45b9ef1b9e
IanFrench <iangordonfrench@gmail.com>
2020-12-08 17:07:22 -0500

    NSURL session added to controller, used obj C error handling site tutorials point to create custom errors

--- Contacts-MRC/Contacts-MRC/ModelControllers/IGFContactsController.m
+++ Contacts-MRC/Contacts-MRC/ModelControllers/IGFContactsController.m
@@ -33,6 +33,57 @@
 
 - (void)randomUsersContacts:(NSString *)users completion:(void (^)(NSError *_Nullable))completion
 {
+    NSURL *baseURL = [NSURL URLWithString:@"https://randomuser.me/api/"];
+    NSURLComponents *components = [NSURLComponents componentsWithURL:baseURL resolvingAgainstBaseURL:YES];
+    NSURLQueryItem *withItem = [NSURLQueryItem queryItemWithName:@"inc" value:@"name,email,phone"];
+    NSURLQueryItem *resultItem = [NSURLQueryItem queryItemWithName:@"results" value:users];
+    components.queryItems = @[withItem, resultItem];
+    NSURL *url = components.URL;
 
+    [[[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+
+        if (error) {
+            NSLog(@"Error: %@", error);
+            completion(error);
+            return;
+        }
+
+        NSError *jsonError = nil;
+        NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+
+        if (jsonError) {
+            NSLog(@"jsonError: %@", jsonError);
+            completion(jsonError);
+            return;
+        }
+
+        if(![dictionary isKindOfClass:[NSDictionary class]]) {
+            NSLog(@"Dictionary error");
+            NSString *domain = @"Invalid domain";
+            NSString *desc = NSLocalizedString(@"Unable to complete the process", @"");
+            NSDictionary *userInfo = @{ NSLocalizedDescriptionKey : desc };
+            NSError *dictionaryError = [NSError errorWithDomain:domain code:-101 userInfo:userInfo];
+            completion(dictionaryError);
+            return;
+        }
+
+
+        NSArray *resultsDictionaries = dictionary[@"results"];
+        for (NSDictionary *usersDictionary in resultsDictionaries) {
+            IGFContacts *contact = [IGFContacts contactWithDictionary:usersDictionary];
+            [self.contacts addObject:contact];
+        }
+        completion(nil);
+
+    }] resume];
+
+
+
+}
+
+- (void)dealloc
+{
+    [_contacts release];
+    [super dealloc];
 }
 @end
