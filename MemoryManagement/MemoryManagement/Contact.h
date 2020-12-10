2ed54b9c3bcddbc4886d9b01a2cf4d369610645b
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-12-10 15:52:15 -0800

    Data model

---
+++ MemoryManagement/MemoryManagement/Contact.h
@@ -0,0 +1,21 @@
+//
+//  Contact.h
+//  MemoryManagement
+//
+//  Created by Cora Jacobson on 12/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Contact : NSObject
+
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *email;
+@property (nonatomic, copy) NSString *phone;
+
+- (instancetype)initWithName:(NSString *)aName email:(NSString *)anEmail phone:(NSString *)aPhone;
+
++ (instancetype)contact;
++ (instancetype)contactWithName:(NSString *)aName email:(NSString *)anEmail phone:(NSString *)aPhone;
+
+@end
